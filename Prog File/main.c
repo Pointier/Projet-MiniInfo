a2c8a5b71fbe9a8ec08584b7b5e45c464c884254
Hugo <hugo.pointier@gmail.com>
2023-01-04 22:15:25 +0100

    Debug Main

--- Prog File/main.c
+++ Prog File/main.c
@@ -8,17 +8,23 @@ int main()
     printf("start main\n");
 
     Case potager[N][N];
-
+    printf("Potager Done\n");
     initialisationCase(potager, N);
-    
-
+    printf("Init Case done\n");
     EnsemblePuc ensP;
+    printf("Ensemble puc done\n");
+
     creaEnsPuc(&ensP);
+    printf("CreaEnsPuc done\n");
     int numPucIni=20;
     initEnsemblePuc(&ensP, numPucIni);
+    printf("Init Ensemble Puc done\n");
+
     initPlacInsecte(&ensP,potager);
+    printf("Init Plac Insecte done\n");
+
     afficher(potager,N);
-      
+    printf("Affichage Done\n");
     
    
 
