56cc0f09a7066da81e79c5eff6bb9470737283cc
Hugo <hugo.pointier@gmail.com>
2023-01-05 09:31:28 +0100

    AA

--- Prog File/initialisation.c
+++ Prog File/initialisation.c
@@ -14,42 +14,46 @@ void initialisationCase(Case tab[N][N], int taille)
     }
 }
 
-void initEnsemblePuc(EnsemblePuc *ensP,int numPuc)
+void initEnsemblePuc(EnsemblePuc *ensP, int numPuc)
 {
     for (int i = 0; i < numPuc; i++)
     {
         Puceron puc;
         initPuc(&puc);
-        ajoutPuc(ensP,&puc);
+        ajoutPuc(ensP, &puc);
     }
 }
 
 void initPlacInsecte(EnsemblePuc *ensP, Case tab[N][N])
 {
     Coordonee tabCoord[ensP->card]; // Penser a rajouter le nombre de coccinelle;
-    int c=0; // Compteur du nombre de coordonnée dans le tableau
-    while (c<ensP->card)
+    int c = 0;                      // Compteur du nombre de coordonnée dans le tableau
+    while (c < ensP->card)
     {
-        bool nouvCoord=true; //
+        bool nouvCoord = true; //
         Coordonee coordRand;
-        coordRand.x = rand() % (N-1);
-        coordRand.y = rand() % (N-1);
-        for (int i=0;i<c;i++)
+        coordRand.x = rand() % (N - 1);
+        coordRand.y = rand() % (N - 1);
+        for (int i = 0; i < c; i++)
         {
-            if (coordRand.x==tabCoord[i].x &&coordRand.y==tabCoord[i].y)
-            nouvCoord=false;
+            if (coordRand.x == tabCoord[i].x && coordRand.y == tabCoord[i].y)
+                nouvCoord = false;
         }
         if (nouvCoord)
         {
-            tabCoord[c]=coordRand;
+            tabCoord[c] = coordRand;
             c++;
         }
-
     }
-    for(int i=0;i<ensP->card;i++)
+    for (int i = 0; i < ensP->card; i++)
     {
-        ensP->tab[i]->coord=tabCoord[i];
-        placementPuc(ensP->tab[i],tab);
+        ensP->tab[i]->coord = tabCoord[i];
+        printf("Coord puc %d x %d y %d", i, ensP->tab[i]->coord.x, ensP->tab[i]->coord.y);
+        placementPuc(ensP->tab[i], tab);
     }
+    for (int i = 0; i < ensP->card; i++)
+    {
 
+        printf("Coord puc %d x %d y %d", i, ensP->tab[i]->coord.x, ensP->tab[i]->coord.y);
+    }
 }
--- Prog File/main.c
+++ Prog File/main.c
@@ -19,12 +19,15 @@ int main()
     initEnsemblePuc(&ensP, numPucIni);
     initPlacInsecte(&ensP, potager);
     // printf("Init Ensemble Puc done\n");
+    printf("Coord puc 0: %d y: %d\n", ensP.tab[0]->coord.x, ensP.tab[0]->coord.y);
+    printf("Coord puc 1: %d y: %d\n", ensP.tab[1]->coord.x, ensP.tab[1]->coord.y);
     printf("Essai avec un puceron hors tableau\n");
     Puceron try;
     initPuc(&try);
     try.coord.x = 2;
     try.coord.y = 3;
     deplacementPuc(&try, potager);
+    
     ensP.tab[0]->coord.x = 5;
     ensP.tab[0]->coord.y = 10;
     placementPuc(ensP.tab[0], potager);
